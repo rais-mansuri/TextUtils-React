0273773610c0fa492d7f7d4535feb1de7c296e2f
Rais <raisahmad.mansuri@aviasole.com>
2023-01-27 12:28:09 +0530

    video 20

--- src/App.js
+++ src/App.js
@@ -21,9 +21,19 @@ function App() {
     }, 1500);
   }
 
-  let toggleMode = () => {
-    if (mode === 'light') {
+  let removeBodyClasses = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-secondary');
+  }
 
+  let toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls);
+    if (mode === 'light') {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
